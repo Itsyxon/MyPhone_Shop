49bced4ee2b63851b71665f972331e2accf67c14
Daniil Itsyxon <daniilitsyxon@gmail.com>
2024-02-26 18:40:16 +0300

    Исправлены ошибки + добавлена кнопка вверх

--- js/script.js
+++ js/script.js
@@ -1,5 +1,17 @@
 const menuBtn = document.querySelector('.menu-btn')
 const menu = document.querySelector('.menu')
+const topBtn = document.querySelector('.gotop')
 menuBtn.addEventListener('click', () => {
   menu.classList.toggle('menu-active')
+})
+
+window.addEventListener('scroll', () => {
+  let scrolled = window.scrollY
+  scrolled >= 1500
+    ? topBtn.classList.add('gotop-show')
+    : topBtn.classList.remove('gotop-show')
+})
+
+topBtn.addEventListener('click', () => {
+  window.scrollTo(0, 0)
 })
